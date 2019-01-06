927e1aaa2d7be6b75336f3ce59d35d630bf44100
Vladislav Kuznecov <super.ewew2015@yandex.ru>
2019-01-06 20:44:29 +0300

    мужчина и женщина объекты

---
+++ src/task/fromJavarush/ManAndWoman.java
@@ -0,0 +1,39 @@
+package task.fromJavarush;
+
+public class ManAndWoman {
+    public static void main(String[] args) {
+        Man man1 = new Man("Iliya", 23, "Glushko");
+        Man man2 = new Man("Vadim", 27, "Zakirova");
+        System.out.println(man1.name + " " + man1.age + " " + man1.address);
+        System.out.println(man2.name + " " + man2.age + " " + man2.address);
+
+        Woman woman1 = new Woman("Masha", 24, "Gorkova");
+        Woman woman2 = new Woman("Yluya", 20, "Vagapova");
+        System.out.println(woman1.name + " " + woman1.age + " " + woman1.address);
+        System.out.println(woman2.name + " " + woman2.age + " " + woman2.address);
+    }
+
+    public static class Man {
+        String name;
+        int age;
+        String address;
+
+        public Man(String name, int age, String address) {
+            this.name = name;
+            this.age = age;
+            this.address = address;
+        }
+    }
+
+    public static class Woman {
+        String name;
+        int age;
+        String address;
+
+        public Woman(String name, int age, String address) {
+            this.name = name;
+            this.age = age;
+            this.address = address;
+        }
+    }
+}
